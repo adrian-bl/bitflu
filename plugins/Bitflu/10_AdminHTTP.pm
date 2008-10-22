d4ef5b7c0fd9c0bb0daac71bbfaf5186a06b0074
Adrian Ulrich <adrian@blinkenlights.ch>
2008-10-22 17:11:44 +0000

    apiversion bump

--- plugins/Bitflu/10_AdminHTTP.pm
+++ plugins/Bitflu/10_AdminHTTP.pm
@@ -9,7 +9,7 @@ package Bitflu::AdminHTTP;
 
 use strict;
 use POSIX;
-use constant _BITFLU_APIVERSION => 20080902;
+use constant _BITFLU_APIVERSION => 20081022;
 
 use constant STATE_READHEADER   => 1;
 use constant STATE_SENDBODY     => 2;
--- plugins/Bitflu/20_DownloadBitTorrent.pm
+++ plugins/Bitflu/20_DownloadBitTorrent.pm
@@ -28,7 +28,7 @@ package Bitflu::DownloadBitTorrent;
 
 use strict;
 use List::Util;
-use constant _BITFLU_APIVERSION => 20080902;
+use constant _BITFLU_APIVERSION => 20081022;
 
 use constant SHALEN   => 20;
 use constant BTMSGLEN => 4;
